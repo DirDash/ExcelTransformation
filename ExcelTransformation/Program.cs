3ef6297d0ca6b06d927406948866a14a729b3c4e
DirDash <dmitry.banokin@gmail.com>
2018-09-18 17:28:06 +0300

    Initial commit.

---
+++ ExcelTransformation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spire.Xls;
+
+namespace ExcelTransformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = "toTransform.xlsx";
+            string outputPath = "result.xlsx";
+
+            Workbook inputWorkbook = new Workbook();
+            inputWorkbook.LoadFromFile(inputPath);
+
+            Workbook outputWorkbook = new Workbook();
+            outputWorkbook.LoadFromFile(outputPath);
+
+            Worksheet sheet = outputWorkbook.Worksheets[0];
+
+            sheet.Range["A1"].Text = "Hello,World!";
+
+            outputWorkbook.Save();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+Spire.XLS for .NET: https://www.e-iceblue.com/Introduce/excel-for-net-introduce.html
+Quick Start: https://www.e-iceblue.com/Tutorials/Spire.XLS/Getting-started/Spire.XLS-Quick-Start.html
