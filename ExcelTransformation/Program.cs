8e96f7507340f8cccd07d607971d04754d0dab3a
DirDash <dmitry.banokin@gmail.com>
2018-09-20 12:06:07 +0300

    Now dlls will be automatically embedded into exe-file. Refactoring.

--- ExcelTransformation/Program.cs
+++ ExcelTransformation/Program.cs
@@ -6,11 +6,26 @@ namespace ExcelTransformation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter file url: ");
+            //Example: C:\Users\DmitryB\Documents\ExcelTransformation\Examples\input2.xlsx
+            Console.WriteLine("Enter (xls|xlsx) file url: ");
             string fileUrl = Console.ReadLine();
 
-            XlsTransformator transformator = new XlsTransformator();
-            transformator.Transform(fileUrl);
+            var xlsNormalizer = new XlsNormalizer();
+            try
+            {
+                xlsNormalizer.NormalizeFile(fileUrl);
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                Console.WriteLine("Press Enter to close the window...");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("Normalization is successfully done.");
+            Console.WriteLine("Press Enter to close the window...");
+            Console.ReadLine();
         }
     }
 }
---
+++ ExcelTransformation/XlsNormalizer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Spire.Xls;
+
+namespace ExcelTransformation
+{
+    public class XlsNormalizer
+    {
+        private string _outputBookFirstColumnTitle = "Id";
+        private string _outputBookSecondColumnTitle = "Manager";
+        private string _outputFileSuffix = "-normalized";
+        private string _outputFileExtension = "xls";
+
+        public void NormalizeFile(string inputFileUrl)
+        {
+            var inputBook = LoadBookFromUrl(inputFileUrl);
+            var outputBook = CreateOutputBook();
+
+            NormalizeSheet(inputBook.Worksheets[0], outputBook.Worksheets[0]);
+
+            SaveOutputBook(outputBook, GetOutputFileUrl(inputBook.FileName));
+        }
+
+        private Workbook LoadBookFromUrl(string fileUrl)
+        {
+            var book = new Workbook();
+            book.LoadFromFile(fileUrl);
+            return book;
+        }
+
+        private Workbook CreateOutputBook()
+        {
+            var outputBook = new Workbook();
+            var firstSheet = outputBook.Worksheets[0];
+
+            firstSheet.Rows[0].Cells[0].Value = _outputBookFirstColumnTitle;
+            firstSheet.Rows[0].Cells[1].Value = _outputBookSecondColumnTitle;
+
+            return outputBook;
+        }
+
+        private void NormalizeSheet(Worksheet inputSheet, Worksheet outputSheet)
+        {
+            int inputRowIndex = 1;
+            int outputRowIndex = 1;
+            while (inputRowIndex < inputSheet.Rows.Length && inputSheet.Rows[inputRowIndex].Cells[0].HasNumber)
+            {
+                string id = inputSheet.Rows[inputRowIndex].Cells[0].Value;
+                int inputCellIndex = 2;
+                while (inputCellIndex < inputSheet.Rows[inputRowIndex].Cells.Length && inputSheet.Rows[inputRowIndex].Cells[inputCellIndex].HasString)
+                {
+                    outputSheet.Rows[outputRowIndex].Cells[0].Value = id;
+                    outputSheet.Rows[outputRowIndex].Cells[1].Value = inputSheet.Rows[inputRowIndex].Cells[inputCellIndex].Value;
+                    outputRowIndex++;
+                    inputCellIndex++;
+                }
+                inputRowIndex++;
+            }
+        }
+
+        private string GetOutputFileUrl(string inputBookFileName)
+        {
+            string outputFileUrl = string.Empty;
+
+            string[] inputFileUrlSplit = inputBookFileName.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < inputFileUrlSplit.Length - 1; i++)
+            {
+                outputFileUrl += inputFileUrlSplit[i];
+            }
+            outputFileUrl += _outputFileSuffix;
+            outputFileUrl += "." + _outputFileExtension;
+
+            return outputFileUrl;
+        }
+
+        private void SaveOutputBook(Workbook outputBook, string fileName)
+        {
+            outputBook.SaveToFile(fileName);
+        }
+    }
+}
